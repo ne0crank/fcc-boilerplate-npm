6b0a32f1b499e1eb20284d4f3387899a413c039d
kent schaeffer <ne0crank@icloud.com>
2023-11-14 14:05:04 -0700

    display /views/html with sendFile, removing junk

--- myApp.js
+++ myApp.js
@@ -1,15 +1,11 @@
 let express = require('express');
 let app = express();
-// app.get("/", (req,res) {
-//   res.send("Hello Express");    
-// });
-//console.log("Hello World");
 
 
 
 
 app.get("/", (req,res) => {
-    res.send("Hello Express");
+    //res.send("Hello Express");
     filePath = __dirname + '/views/index.html';  
     res.sendFile(filePath);  
   });
